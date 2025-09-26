5a6721bdcf6c6545f1d5a5a5d5c455c77a6c221a
Matuzh <98213749+Matuzh@users.noreply.github.com>
2025-09-26 19:00:02 +0200

    Initialize Next.js app with Tailwind CSS setup
    Set up initial Next.js project structure, add Tailwind CSS configuration, and basic global styles. Added initial layout and home page components, TypeScript config and type declarations, and moved dashboard page to correct directory. Updated dependencies to include Tailwind CSS, Radix UI, React Query, Zod, Zustand, and related packages.
    Created basic Hello World page in app/page.tsx.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./compontents/**/*.{js,ts,jsx,tsx,mdx}",
+        "./hooks/**/*.{js,ts,jsx,tsx,mdx}",
+        "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+        "./styles/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    };
